56ccc678c7f5f25e0122751848cb4d05c01dfc33
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-14 18:13:25 -0500

    added functionality to add new blocks

--- blockchain.js
+++ blockchain.js
@@ -9,4 +9,14 @@ class Blockchain {
     generateGenesisBlock(){
         return new Block(0, '07/11/2019', "This is the genesis block", "0")
     }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().previousHash
+        newBlock.hash = newBlock.calculateHash()
+        this.chain.push(newBlock)
+    }
 }
